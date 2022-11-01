3e6e9512fbd3a62ce5bffb94385c7ecf0ad90293
srsviegas <11709348+srsviegas@users.noreply.github.com>
2022-11-01 19:01:06 -0300

    Add PERM

---
+++ bioinformatics-stronghold/perm.py
@@ -0,0 +1,22 @@
+""" PERM : Enumerating Gene Orders
+A permutation of length n is an ordering of the positive integers {1,2,…,n}. For
+example, π=(5,3,2,1,4) is a permutation of length 5.
+
+Given: A positive integer n ≤ 7.
+Return: The total number of permutations of length n, followed by a list of all
+such permutations (in any order).
+"""
+
+
+from itertools import permutations
+
+
+if __name__ == "__main__":
+    lenght = int(input())
+    perm_list = list(permutations(range(1, lenght+1)))
+    with open("perm_out.txt", "w") as output:
+        output.write(f'{len(perm_list)}')
+        for perm in perm_list:
+            output.write('\n')
+            for integer in perm:
+                output.write(f'{integer} ')
