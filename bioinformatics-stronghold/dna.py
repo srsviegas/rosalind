19966c8140c9122e8b6e53feb9ea812d2fc60dc6
srsviegas <11709348+srsviegas@users.noreply.github.com>
2022-11-01 16:52:19 -0300

    Add DNA

---
+++ bioinformatics-stronghold/dna.py
@@ -0,0 +1,15 @@
+""" DNA | Counting DNA Nucleotides
+Given: A DNA string s of length at most 1000 nt.
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols
+'A', 'C', 'G', and 'T' occur in s.
+"""
+
+
+def count_molecules(dna_string):
+    return map(dna_string.count, "ACGT")
+
+
+if __name__ == "__main__":
+    with open("bioinformatics-stronghold\dna.txt", "r") as dataset:
+        n_molecules = count_molecules(dataset.readline())
+    print(*n_molecules)
