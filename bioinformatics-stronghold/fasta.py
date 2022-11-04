7360436501f93110737bc68810081273bd7bf6d5
srsviegas <11709348+srsviegas@users.noreply.github.com>
2022-11-03 21:28:55 -0300

    Add search_uniprot to fasta.py

--- bioinformatics-stronghold/fasta.py
+++ bioinformatics-stronghold/fasta.py
@@ -2,6 +2,7 @@
 
 
 from re import split
+import requests
 
 
 def readfasta(filename):
@@ -13,3 +14,9 @@ def readfasta(filename):
     with open(filename, 'r') as file:
         strings = split('>.*\n', file.read())
     return [s.replace('\n', '') for s in strings[1:]]
+
+
+def search_uniprot(uniprot_id):
+    url = f"http://www.uniprot.org/uniprot/{uniprot_id}.fasta"
+    response = requests.get(url).content.decode('utf-8')
+    return split('\n', response, 1)[1].replace('\n', '')
