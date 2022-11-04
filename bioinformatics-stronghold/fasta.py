75007e8df241de76e0924ad77c671d09b5b587eb
srsviegas <11709348+srsviegas@users.noreply.github.com>
2022-11-03 21:02:10 -0300

    Optimize readfasta

--- bioinformatics-stronghold/fasta.py
+++ bioinformatics-stronghold/fasta.py
@@ -1,24 +1,15 @@
 # Module for reading FASTA files
 
+
+from re import split
+
+
 def readfasta(filename):
     """Reads files in FASTA format.
     
     Every string in a FASTA file begins with a single-line that contains
     the symbol '>' along with some labeling information about the string.
     """
-    with open(filename, "r") as file:
-        lines = file.readlines()
-    string_list = []
-    aux_str = ""
-    for line in lines[1:]:
-        line = line.replace('\n', '')
-        if line[0] == '>':
-            string_list.append(aux_str)
-            aux_str = ""
-        else:
-            aux_str = "".join([aux_str, line])
-    string_list.append(aux_str)
-    if len(string_list) == 1:
-        return string_list[0]
-    else:
-        return string_list
+    with open(filename, 'r') as file:
+        strings = split('>.*\n', file.read())
+    return [s.replace('\n', '') for s in strings[1:]]
